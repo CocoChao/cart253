a2c68768d98b49309dac941df004240c9e583013
Carole Chao <carolechao@hotmail.com>
2019-09-10 17:24:09 -0400

    Update script.js

--- template-p5-project/template-p5-project/js/script.js
+++ template-p5-project/template-p5-project/js/script.js
@@ -1,18 +1,20 @@
-/*****************
+//Title of Project
+//Author Name
 
-Title of Project
-Author Name
+//This is a template. You must fill in the title,
+//author, and this description to match your project!
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
-
-******************/
+//
 
 // preload()
 //
 // Description of preload
 
 function preload() {
+
+let x = 0;
+let y = 0;
+let size = 1
 
 }
 
@@ -22,6 +24,11 @@ function preload() {
 // Description of setup
 
 function setup() {
+createCanvas(500,500);
+background(145,200,639);
+fill(200,159,80);
+noStroke();
+
 
 }
 
@@ -31,5 +38,10 @@ function setup() {
 // Description of draw()
 
 function draw() {
+
+x += 5;
+y += 5;
+size += 2;
+ellipse(0,0,1,1);
 
 }
