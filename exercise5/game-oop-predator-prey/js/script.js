4b415ce9512469289879ccc358e7db2480e87296
Carole Chao <carolechao@hotmail.com>
2019-10-21 15:48:16 -0400

    Ex5: Add 2 more predators to the script in all the functions

--- exercise5/game-oop-predator-prey/js/script.js
+++ exercise5/game-oop-predator-prey/js/script.js
@@ -1,12 +1,15 @@
 // Predator-Prey Simulation
 // by Pippin Barr
+// Modified by Carole Chao
 //
-// Creates a predator and three prey (of different sizes and speeds)
-// The predator chases the prey using the arrow keys and consumes them.
-// The predator loses health over time, so must keep eating to survive.
+// Creates three predators and three preys (of different sizes and speeds)
+// The predators chases the prey using the arrow keys and consumes them.
+// The predators loses health over time, so must keep eating to survive.
 
-// Our predator
+// Our predators
 let tiger;
+let leopard;
+let bear;
 
 // The three prey
 let antelope;
@@ -20,6 +23,8 @@ let bee;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   tiger = new Predator(100, 100, 5, color(200, 200, 0), 40);
+  leopard = new Predator(100, 100, 15, color(200, 175, 175), 35);
+  bear = new Predator(100, 100, 25, color(240, 240, 150), 45);
   antelope = new Prey(100, 100, 10, color(255, 100, 10), 50);
   zebra = new Prey(100, 100, 8, color(255, 255, 255), 60);
   bee = new Prey(100, 100, 20, color(255, 255, 0), 10);
@@ -32,11 +37,16 @@ function draw() {
   // Clear the background to black
   background(0);
 
-  // Handle input for the tiger
+  // Handle input for the predators
   tiger.handleInput();
+  leopard.handleInput();
+  bear.handleInput();
+
 
   // Move all the "animals"
   tiger.move();
+  leopard.move();
+  bear.move();
   antelope.move();
   zebra.move();
   bee.move();
@@ -46,8 +56,20 @@ function draw() {
   tiger.handleEating(zebra);
   tiger.handleEating(bee);
 
+  // Handle the leopard eating any of the prey
+  leopard.handleEating(antelope);
+  leopard.handleEating(zebra);
+  leopard.handleEating(bee);
+
+  // Handle the bear eating any of the prey
+  bear.handleEating(antelope);
+  bear.handleEating(zebra);
+  bear.handleEating(bee);
+
   // Display all the "animals"
   tiger.display();
+  leopard.display();
+  bear.display();
   antelope.display();
   zebra.display();
   bee.display();
