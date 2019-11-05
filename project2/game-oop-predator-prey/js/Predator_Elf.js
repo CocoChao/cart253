7ea1c778b7cb4b25e2535180c9ad19db9fc21f6e
Carole Chao <carolechao@hotmail.com>
2019-11-05 13:08:47 -0500

    P2: Added 2 images for the 2 classes
    An Apple, fruit for the class Food
    An Elf, character for the class Creatures

--- project2/game-oop-predator-prey/js/Predator_Elf.js
+++ project2/game-oop-predator-prey/js/Predator_Elf.js
@@ -1,10 +1,98 @@
-class Creatures extends Elf {
+class Elf extends Creatures {
 
   // constructor
   //
   // Sets the initial values for the Predator's properties
   // Either sets default values or uses the arguments provided
   constructor(x, y, speed, fillColor, radius, animalImage) {
-
+    // Position
+    this.x = x;
+    this.y = y;
+    // Velocity and speed
+    this.vx = 0;
+    this.vy = 0;
+    this.speed = speed;
+    // Health properties
+    this.maxHealth = radius;
+    this.health = this.maxHealth;
+    this.healthLossPerMove = 0.1;
+    this.healthGainPerEat = 1;
+    // Display properties
+    this.fillColor = fillColor;
+    this.radius = this.health;
+    // Input properties
+    this.upKey = up;
+    this.downKey = down;
+    this.leftKey = left;
+    this.rightKey = right;
+    this.shiftKey = run;
+    this.score = 0;
+    this.animalImage = animalImage;
   }
-}
+  // Both the endpoint coordinates should jiggle
+  update() {
+    super.update(); // Do the generic Shape update()
+    this.x2 += random(-1, 1);
+    this.y2 += random(-1, 1);
+  }
+  // move
+  //
+  // Updates the position according to velocity
+  // Lowers health (as a cost of living)
+  // Handles wrapping
+  move() {
+    // Update position
+    this.x += this.vx;
+    this.y += this.vy;
+    // Update health
+    this.health = this.health - this.healthLossPerMove;
+    this.health = constrain(this.health, 0, this.maxHealth);
+    // Handle wrapping
+    this.handleWrapping();
+  }
+  // handleWrapping
+  //
+  // Checks if the predator has gone off the canvas and
+  // wraps it to the other side if so
+  handleWrapping() {
+    // Off the left or right
+    if (this.x < 0) {
+      this.x += width;
+    } else if (this.x > width) {
+      this.x -= width;
+    }
+    // Off the top or bottom
+    if (this.y < 0) {
+      this.y += height;
+    } else if (this.y > height) {
+      this.y -= height;
+    }
+  }
+  // handleEating
+  //
+  // Takes a Prey object as an argument and checks if the predator
+  // overlaps it. If so, reduces the prey's health and increases
+  // the predator's. If the prey dies, it gets reset.
+  handleEating(prey) {
+    // Calculate distance from this predator to the prey
+    let d = dist(this.x, this.y, prey.x, prey.y);
+    // Check if the distance is less than their two radius (an overlap)
+    if (d < this.radius + prey.radius) {
+      // Increase predator health and constrain it to its possible range
+      this.health += this.healthGainPerEat;
+      this.health = constrain(this.health, 0, this.maxHealth);
+      // Play our crinkle sound effect by rewinding and then playing
+      crinkleSFX.currentTime = 0;
+      crinkleSFX.play();
+      // Decrease prey health by the same amount
+      prey.health -= this.healthGainPerEat;
+      // Check if the prey died and reset it if so
+      if (prey.health < 0) {
+        prey.reset();
+        // // Check if the predator dies
+        // if (this.health = 0){
+        //   predator.reset();
+        //////////////////
+      }
+    }
+  }
--- project2/game-oop-predator-prey/js/script.js
+++ project2/game-oop-predator-prey/js/script.js
@@ -10,19 +10,23 @@
 let tigerImage;
 let pigImage;
 let bearImage;
+let elfImage;
 
 let tiger;
 let pig;
 let bear;
+let elf;
 
-// The three sweets
+// The Food: the Sweets and Fruits
 let lollipopImage;
 let candyImage;
 let cottonImage;
+let appleImage;
 
 let lollipop;
 let candy;
 let cotton;
+let apple;
 
 // Add variable for font
 let emilysCandy;
@@ -31,26 +35,33 @@ let emilysCandy;
 // eats a candy
 let crinkleSFX;
 
+// A variable to add the start screen and ending screen
 let startGame = false;
+let gameOver = false;
 
 // preload()
 //
 // Preload pictures of the creatures (the tiger, the pig and the bear),
-// the sweets (the lollipop, the candy and the cotton) and the background pictures
+// the Food (the lollipop, the candy and the cotton) and the background pictures
 // for the Start Screen and Play Screen.
 
 function preload() {
- tigerImage = loadImage("assets/images/tiger-Disney-tsum-tsum.png");
- pigImage = loadImage("assets/images/disney-tsum-tsum-piglet.png");
- bearImage = loadImage("assets/images/disney-tsum-tsum-clipart-winnie-the-pooh.png");
- lollipopImage = loadImage("assets/images/candy-lollipop.png");
- candyImage = loadImage("assets/images/clipart-candy-dulce-vector-png.png");
- cottonImage = loadImage("assets/images/cotton-candy-clipart.png");
- backdrop = loadImage("assets/images/candyland_background.png");
- startScreenBackdrop = loadImage("assets/images/start_screen-candy-background.jpg");
- endScreenBackdrop = loadImage("assets/images/candyland-clipart-path-2.png");
- emilysCandy = loadFont("assets/images/Emilys_Candy/EmilysCandy-Regular.ttf");
- crinkleSFX = new Audio("assets/sounds/candy-wrapper-crinkle.wav");
+//Preload Predators Image
+  tigerImage = loadImage("assets/images/tiger-Disney-tsum-tsum.png");
+  pigImage = loadImage("assets/images/disney-tsum-tsum-piglet.png");
+  bearImage = loadImage("assets/images/disney-tsum-tsum-clipart-winnie-the-pooh.png");
+  elfImage = loadImage("assets/images/Christmas_Elf_with_Santa_Bag_Clipart.png");
+//Preload Preys Image
+  lollipopImage = loadImage("assets/images/candy-lollipop.png");
+  candyImage = loadImage("assets/images/clipart-candy-dulce-vector-png.png");
+  cottonImage = loadImage("assets/images/cotton-candy-clipart.png");
+  appleImage = loadImage("assets/images/cartoon-apple-transparent.jpg");
+//Preload Background, font and sound
+  backdrop = loadImage("assets/images/candyland_background.png");
+  startScreenBackdrop = loadImage("assets/images/start_screen-candy-background.jpg");
+  endScreenBackdrop = loadImage("assets/images/candyland-clipart-path-2.png");
+  emilysCandy = loadFont("assets/images/Emilys_Candy/EmilysCandy-Regular.ttf");
+  crinkleSFX = new Audio("assets/sounds/candy-wrapper-crinkle.wav");
 }
 
 // setup()
@@ -60,15 +71,19 @@ function preload() {
 // Set the text font and sound effects every time a Predator eats Prey
 
 function setup() {
-  createCanvas(1300,1000);
+  createCanvas(1300, 1000);
   textFont(emilysCandy);
   crinkleSFX.play();
   tiger = new Creatures(100, 100, 5, color(200, 200, 0), 40, tigerImage, UP_ARROW, DOWN_ARROW, LEFT_ARROW, RIGHT_ARROW, 16); // press Shift key to sprint);
-  pig = new Creatures(100,100,15, color(150,150,150),30, pigImage, 87, 83, 65, 68, 70); // press F to sprint);
-  bear = new Creatures(100,100,20, color(120, 114, 97),45, bearImage, 73, 75, 74, 76, 72); // press H key to sprint);
-  lollipop = new Sweets(100, 100, 10, color(255, 100, 10), 50, lollipopImage);
-  candy = new Sweets(100, 100, 8, color(255, 255, 255), 60, candyImage);
-  cotton = new Sweets(100, 100, 20, color(255, 255, 0), 10, cottonImage);
+  pig = new Creatures(100, 100, 15, color(150, 150, 150), 30, pigImage, 87, 83, 65, 68, 70); // press F to sprint);
+  bear = new Creatures(100, 100, 20, color(120, 114, 97), 45, bearImage, 73, 75, 74, 76, 72); // press H key to sprint);
+  elf = new Creatures(100,100,20, color(100,100,100),35, elfImage);
+  creatures.push(elf);
+  lollipop = new Food(100, 100, 10, color(255, 100, 10), 50, lollipopImage);
+  candy = new Food(100, 100, 8, color(255, 255, 255), 60, candyImage);
+  cotton = new Food(100, 100, 20, color(255, 255, 0), 10, cottonImage);
+  apple = new Fruit(100,100,25, color(255,220,225),55, appleImage);
+  food.push(apple);
 }
 
 // draw()
@@ -76,82 +91,102 @@ function setup() {
 // Handles input, movement, eating, and displaying for the system's objects
 function draw() {
 
-if(startGame === true){
-  // Start Screen with game appears
-  background(backdrop,0,0);
-  // Handle input for the tiger
-  tiger.handleInput();
-  pig.handleInput();
-  bear.handleInput();
+  if (startGame === true) {
+    // Start Screen with game appears
+    background(backdrop, 0, 0);
+    // Handle input for all the predators
+    tiger.handleInput();
+    pig.handleInput();
+    bear.handleInput();
 
-  // Move all the "animals"
-  tiger.move();
-  pig.move();
-  bear.move();
-  lollipop.move();
-  candy.move();
-  cotton.move();
+    // Move all the "animals"
+    tiger.move();
+    pig.move();
+    bear.move();
+    elf.move();
+    lollipop.move();
+    candy.move();
+    cotton.move();
+    apple.move();
 
-  // Handle the tiger eating any of the Sweets
-  tiger.handleEating(lollipop);
-  tiger.handleEating(candy);
-  tiger.handleEating(cotton);
+    // Handle the tiger eating any Food
+    tiger.handleEating(lollipop);
+    tiger.handleEating(candy);
+    tiger.handleEating(cotton);
+    tiger.handleEating(apple);
 
-  // Handle the pig eating any of the Sweets
-  pig.handleEating(lollipop);
-  pig.handleEating(candy);
-  pig.handleEating(cotton);
+    // Handle the pig eating any Food
+    pig.handleEating(lollipop);
+    pig.handleEating(candy);
+    pig.handleEating(cotton);
+    pig.handleEating(apple);
 
-  // Handle the bear eating any of the Sweets
-  bear.handleEating(lollipop);
-  bear.handleEating(candy);
-  bear.handleEating(cotton);
+    // Handle the bear eating any Food
+    bear.handleEating(lollipop);
+    bear.handleEating(candy);
+    bear.handleEating(cotton);
+    bear.handleEating(apple);
 
-  // Display all the "animals"
-  tiger.display();
-  pig.display();
-  bear.display();
-  lollipop.display();
-  candy.display();
-  cotton.display();
-  }
-  else{
+    // Handle the Evil Elves eating any Food
+    elf.handleEating(lollipop);
+    elf.handleEating(candy);
+    elf.handleEating(cotton);
+    elf.handleEating(apple);
+
+    // Display all the "animals"
+    tiger.display();
+    pig.display();
+    bear.display();
+    elf.display();
+    lollipop.display();
+    candy.display();
+    cotton.display();
+    apple.display();
+  } else {
     // Start Screen image of Candy themed background with Game instructions
     // and title.
-    background(startScreenBackdrop,0,0);
+    background(startScreenBackdrop, 0, 0);
     fill(227, 101, 91);
     textSize(75);
     textAlign(CENTER);
-    text("SWEET SUGAR CANDYLAND",width/2, height/2);
+    text("SWEET SUGAR CANDYLAND", width / 2, height / 2);
     textSize(30);
     textAlign(CENTER);
-    text("Press Spacebar to play.", width/2, height/2 + 50);
-    text("Use keys Up, Down, Right, Left to move Tiger", width/2, height/2 +100);
-    text("Use keys W, A, S, D to move Pig.", width/2, height/2 + 150);
-    text("Use keys I, J, K, L to move Winnie the Pooh.\n GOOD LUCK!.",width/2, height/2 +200);
+    text("Press Spacebar to play.", width / 2, height / 2 + 50);
+    text("Use keys Up, Down, Right, Left to move Tiger", width / 2, height / 2 + 100);
+    text("Use keys W, A, S, D to move Pig.", width / 2, height / 2 + 150);
+    text("Use keys I, J, K, L to move Winnie the Pooh.\n GOOD LUCK!.", width / 2, height / 2 + 200);
+  } if (gameOver === true){
+    // Display the CandyLand path on the screen with Game Over text
+    textFont(emilysCandy);
+    textSize(45);
+    textAlign(CENTER, CENTER);
+    fill(227, 101, 91);
+    image(endScreenBackdrop, 0, 0);
+
+    // Set up the text to display
+    let gameOverText = "POUF! They dissapeared!\n"; // \n means "new line"
+    gameOverText = gameOverText + "You ate " + this.score + " candies\n";
+    gameOverText = gameOverText + "before you left Candyland."
+    // Display it in the centre of the screen
+    text(gameOverText, width / 2, height / 2);
   }
 }
 
 // Start playing when pressing Spacebar
-function keyPressed(){
-if(keyCode == 32){
-  startGame = true;
+function keyPressed() {
+  if (keyCode == 32) {
+    startGame = true;
+  }
 }
+  // Show game over screen when predator dies
+  function showGameOver() {
+    if (predator.health = 0) {
+      gameOver = true;
+    }
+  }
 //
-function showGameOver() {
-  // Set up the font
-  textFont(emilysCandy);
-  textSize(45);
-  textAlign(CENTER, CENTER);
-  fill(227, 101, 91);
-  // Display the glitched TV screen image on the screen
-  image(endScreenBackdrop, 0, 0);
-
-  // Set up the text to display
-  let gameOverText = "POUF! They dissapeared!\n"; // \n means "new line"
-  gameOverText = gameOverText + "You ate " + this.score + " candies\n";
-  gameOverText = gameOverText + "before you left Candyland."
-  // Display it in the centre of the screen
-  text(gameOverText, width / 2, height / 2);
-}
-}
+for (let i = 0; i < shapes.length; i++) {
+    shapes[i].update();
+    shapes[i].display();
+  }
