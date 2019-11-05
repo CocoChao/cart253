36359b70ff8b7d70a065cd4bbbdc32f442824411
Carole Chao <carolechao@hotmail.com>
2019-11-05 11:30:37 -0500

    P2: Added two new classes
    And added a ending screen (need to debug/fix)

---
+++ project2/game-oop-predator-prey/js/Predator_Elf.js
@@ -0,0 +1,10 @@
+class Creatures extends Elf {
+
+  // constructor
+  //
+  // Sets the initial values for the Predator's properties
+  // Either sets default values or uses the arguments provided
+  constructor(x, y, speed, fillColor, radius, animalImage) {
+
+  }
+}
