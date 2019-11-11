a72f53f44406f4b646ca072108497ef6aad5fc2f
Carole Chao <carolechao@hotmail.com>
2019-11-11 14:15:22 -0500

    P3: Added/created files
    + moved files in folder
    + modified the aesthetic

---
+++ project3/js/script.js
@@ -0,0 +1,28 @@
+// Project 3 - Prototype 1
+// by Carole Chao
+//
+//
+// Creates predators, preys and obstacles (of different sizes and speeds)
+// The predator chase the preys using the arrows and keys to consume them.
+// The predator loses health over time, so much keep eating to survive.
+
+// Set variable for cars
+let skyBlueCarImage;
+let darkBlueImage;
+let yellowCarImage;
+let redCarImage;
+
+// Set variables for the car tools and obstacles
+let tool1;
+let tool2;
+let tool3;
+let tool4;
+let tool5;
+let tool6;
+let tool7;
+let tool8;
+let tool9;
+
+// Add variables for font
+
+// A variable for the car sounds & background music and sounds.
