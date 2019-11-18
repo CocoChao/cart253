5eb80bfe5537bbd5862a810f55894379f4bbd20e
Carole Chao <carolechao@hotmail.com>
2019-11-18 11:18:53 -0500

    Ex8: Start Exercise 8

---
+++ exercise8/js/Vehicle.js
@@ -0,0 +1,146 @@
+// Vehicule
+//
+// A class that represents a simple vehicule
+// controlled by the arrow keys. It can move around
+// the screen and sonsume Instrument (Prey) objects to maintain its health.
+
+class Vehicle {
+
+// Constructor
+//
+// Sets the initial values for the Vehicle's properties
+// Either sets default values or uses the arguments provided
+// Vehicles are skyBlueCar, darkBlueCar, yellowCar and redCar
+constructor(x, y, speed, fillColor, radius, elementImage, up, down, left, right, sprint) {
+    // Position
+    this.x = x;
+    this.y = y;
+    // Velocity and speed
+    this.vx = 0;
+    this.vy = 0;
+    this.speed = speed;
+    // Health properties
+    this.maxHealth = radius;
+    this.health = this.maxHealth;
+    this.healthLossPerMove = 0.1;
+    this.healthGainPerEat = 1;
+    // Display properties
+    this.fillColor = fillColor;
+    this.radius = this.health;
+    // Input properties
+    this.upKey = up;
+    this.downKey = down;
+    this.leftKey = left;
+    this.rightKey = right;
+    this.shiftKey = sprint;
+    this.score = 0;
+    this.elementImage = elementImage;
+  }
+
+  // handleInput
+  //
+  // Checks if an arrow key is pressed and sets the predator's
+  // velocity appropriately.
+  handleInput() {
+    // Horizontal movement
+    if (keyIsDown(this.leftKey)) {
+      this.vx = -this.speed;
+    } else if (keyIsDown(this.rightKey)) {
+      this.vx = this.speed;
+    } else {
+      this.vx = 0;
+    }
+    // Vertical movement
+    if (keyIsDown(this.upKey)) {
+      this.vy = -this.speed;
+    } else if (keyIsDown(this.downKey)) {
+      this.vy = this.speed;
+    } else {
+      this.vy = 0;
+    }
+    // Add the ability to "sprint" when the player holds down the shift key
+    if (keyIsDown(this.shiftKey)) {
+      this.speed = this.speed * 1.5;
+    } else {
+      this.speed = this.speed;
+    }
+  }
+
+  // move
+  //
+  // Updates the position according to velocity
+  // Lowers health (as a cost of living)
+  // Handles wrapping
+  move() {
+    // Update position
+    this.x += this.vx;
+    this.y += this.vy;
+    // Update health
+    this.health = this.health - this.healthLossPerMove;
+    this.health = constrain(this.health, 0, this.maxHealth);
+    // Handle wrapping
+    this.handleWrapping();
+  }
+
+  // handleWrapping
+  //
+  // Checks if the predator has gone off the canvas and
+  // wraps it to the other side if so
+  handleWrapping() {
+    // Off the left or right
+    if (this.x < 0) {
+      this.x += width;
+    } else if (this.x > width) {
+      this.x -= width;
+    }
+    // Off the top or bottom
+    if (this.y < 0) {
+      this.y += height;
+    } else if (this.y > height) {
+      this.y -= height;
+    }
+  }
+
+  // handleEating
+  //
+  // Takes a Prey object as an argument and checks if the predator
+  // overlaps it. If so, reduces the prey's health and increases
+  // the predator's. If the prey dies, it gets reset.
+  handleEating(instrument) {
+    // Calculate distance from this predator to the prey
+    let d = dist(this.x, this.y, instrument.x, instrument.y);
+    // Check if the distance is less than their two radii (an overlap)
+    if (d < this.radius + instrument.radius) {
+      // Increase predator health and constrain it to its possible range
+      this.health += this.healthGainPerEat;
+      this.health = constrain(this.health, 0, this.maxHealth);
+      // Decrease prey health by the same amount
+      instrument.health -= this.healthGainPerEat;
+      // Check if the prey died and reset it if so
+      if (instrument.health < 0) {
+        instrument.reset();
+        this.score++;
+      }
+    }
+  }
+
+  // display
+  //
+  // Draw the predator as an ellipse on the canvas
+  // with a radius the same size as its current health.
+  display() {
+    if (this.health === 0){
+      return;
+    }
+    image(this.elementImage, this.x, this.y, this.radius * 2, this.radius * 2);
+    push();
+    noStroke();
+    fill(this.fillColor);
+    this.radius = this.health;
+    pop();
+    // Add the score on top of the predators
+    textSize(30);
+    fill(71, 71, 71);
+    text("Score: " + this.score, this.x, this.y);
+}
+}
---
+++ exercise8/js/script.js
@@ -0,0 +1,182 @@
+// Project 3 - Prototype 1
+// Previous code from Exercise 5 and Exercise 7
+// by Pippin Barr
+// Modified by Carole Chao
+//
+//
+// Creates predators, preys and obstacles (of different sizes and speeds)
+// The predator chase the preys using the arrows and keys to consume them.
+// The predator loses health over time, so much keep eating to survive.
+
+// Set variable for cars
+let skyBlueCarImage;
+let darkBlueImage;
+let yellowCarImage;
+let redCarImage;
+
+let skyBlueCar;
+let darkBlueCar;
+let yellowCar;
+let redCar;
+
+// Set variables for the car tools and obstacles
+let toolOneImage;
+let toolTwoImage;
+let toolThreeImage;
+let toolFourImage;
+let toolFiveImage;
+let barriersImage;
+
+let toolOne;
+let toolTwo;
+let toolThree;
+let toolFour;
+let toolFive;
+let barriers;
+
+// Add variables for font
+let wallpoet;
+
+// A variable for the car sounds & background music and sounds.
+
+
+// A variable to add the start screen and ending screen.
+let startGame = false;
+let gamerOver = false;
+
+// Add empty groups arrays for cars and tools
+let vehiclesGroup = [];
+let toolsGroups = [];
+let obstacles = [];
+
+// preload()
+//
+// Preload pictures of all the elements, images, background sound, font
+// (the cars, the tools, the obstacle), the background pictures
+// and background music & soundFX for the Start Screen, Play Screen and Game Over screen.
+
+function preload(){
+  //Preload cars Images
+  skyBlueCarImage = loadImage("assets/images/Race-Car-PNG-Picture.png");
+  darkBlueImage = loadImage("assets/images/car-racing-clipart-10.jpg.png");
+  yellowCarImage = loadImage("assets/images/Race-Car-PNG-Clipart.png");
+  redCarImage = loadImage("assets/images/Race-Car-PNG-HD.png");
+  // Preload tool images
+  toolOneImage = loadImage("assets/images/Tool1.png");
+  toolTwoImage = loadImage("assets/images/tool2.png");
+  toolThreeImage = loadImage("assets/images/tool3.png");
+  toolFourImage = loadImage("assets/images/tool4.png");
+  toolFiveImage = loadImage("assets/images/tool5.png");
+
+  // Preload obstacle images
+  barriers = loadImage("assets/images/clip-art-vector-construction.png");
+  // Preload Background, font, sounds and music
+  backdrop = loadImage("assets/images/F1-Track_backgroundimage.jpg");
+  startScreenBackdrop = loadImage("assets/images/F1-Track_backgroundimage.jpg");
+  endScreenBackdrop = loadImage("assets/images/Game-over-screen.jpg");
+  wallpoet = loadFont("assets/images/Wallpoet/Wallpoet-Regular.ttf");
+  // new Audio("assets/sounds/")
+
+}
+
+// setup()
+//
+// Set up a canvas
+// Creates objects for the the game (predator and preys)
+// Set the text font and sound effects every time a Predator eats Prey
+
+function setup(){
+  console.log("loaded");
+  createCanvas(1280,720);
+  background(backdrop,0,0);
+  textFont(wallpoet);
+// put the cars in array groups
+  skyBlueCar = new Vehicle(100,100,5,color(200,200,0),50, skyBlueCarImage, UP_ARROW, DOWN_ARROW, LEFT_ARROW, RIGHT_ARROW, 16);
+  darkBlueCar = new Vehicle(100,100,15,color(150,150,150), 50, darkBlueImage, 87, 83, 65, 68, 70);
+  yellowCar = new Vehicle(100,100,10,color(120,120,97),50, yellowCarImage, 73, 75, 74, 76, 72);
+  redCar = new Instrument(100,100,15,color(175,175,175),50,redCarImage);
+// put the tools in array groups
+  toolOne = new Instrument(100,100,14, color(255,100,10),25, toolOneImage);
+  toolTwo = new Instrument(100,100,10, color(255,255,255),25, toolTwoImage);
+  toolThre = new Instrument(100,100,17, color(255,255,0),25, toolThreeImage);
+  toolFour = new Instrument(100,100,14, color(255,255,255),25, toolFourImage);
+  toolFive = new Instrument(100,100,15, color(255,255,255),25, toolFiveImage);
+  // barriers = new Obstacle(100,100,15,color(255,255,255),40, barriersImage);
+}
+
+// draw()
+// Add the ability to start playing when Spacebar is pressed and change screens
+// Handles input, movement, eating, and displaying for the system's objects
+function draw() {
+// Add background image
+background(backdrop,0,0);
+
+// Handle input for the cars/predators
+skyBlueCar.handleInput();
+darkBlueCar.handleInput();
+yellowCar.handleInput();
+// redCar.handleInput();
+
+// Move all the cars in the game
+skyBlueCar.move();
+darkBlueCar.move();
+yellowCar.move();
+redCar.move();
+
+// Move all the tools and obstacle in the game
+toolOne.move();
+toolTwo.move();
+// toolThree.move();
+toolFour.move();
+toolFive.move();
+// barriers.move();
+
+
+// Handle skyBlueCar to eat any type of tools + obstacle
+skyBlueCar.handleEating(toolOne);
+skyBlueCar.handleEating(toolTwo);
+// skyBlueCar.handleEating(toolThree);
+skyBlueCar.handleEating(toolFour);
+skyBlueCar.handleEating(toolFive);
+
+// skyBlueCar.handleEating(barriers);
+
+// Handle darkBlueCar to eat any type of tools + obstacle
+darkBlueCar.handleEating(toolOne);
+darkBlueCar.handleEating(toolTwo);
+// darkBlueCar.handleEating(toolThree);
+darkBlueCar.handleEating(toolFour);
+darkBlueCar.handleEating(toolFive);
+
+// darkBlueCar.handleEating(barriers);
+
+// Handle yellowCar to eat any type of tools + obstacle
+yellowCar.handleEating(toolOne);
+yellowCar.handleEating(toolTwo);
+// yellowCar.handleEating(toolThree);
+yellowCar.handleEating(toolFour);
+yellowCar.handleEating(toolFive);
+
+// yellowCar.handleEating(barriers);
+
+// // Handle redCar to eat any type of tools + obstacle
+// redCar.handleEating(toolOne);
+// redCar.handleEating(toolTwo);
+// // redCar.handleEating(toolThree);
+// redCar.handleEating(toolFour);
+// redCar.handleEating(toolFive);
+
+// redCar.handleEating(barriers);
+
+// Display all the elements on the screen
+skyBlueCar.display();
+darkBlueCar.display();
+redCar.display();
+yellowCar.display();
+toolOne.display();
+toolTwo.display();
+// toolThree.display();
+toolFour.display();
+toolFive.display();
+// barriers.display();
+}
